d4f0f5d845b82aeffd2fa07399cce4d1fbfa9d70
percytataje <ptataje@znerg.com>
2018-10-08 02:14:10 -0500

    e3

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express =require('express');
 const jsonGraphqlExpress =require ('json-graphql-server');
 const datafake = require('./db');
 
-const PORT = 8000;
 const app = express();
 const data = datafake;
 app.use('/', jsonGraphqlExpress.default(data));
-app.listen(PORT);
+app.listen(process.env.PORT || 5000);
